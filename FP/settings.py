0056dd05d2df815c56d6ef25bccd97d3e7308abc
ajayaj2000 <ajaysri2016@gmail.com>
2022-06-05 22:09:41 +0530

    trying to deploy

--- FP/settings.py
+++ FP/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-7mnw)9yxdg(_68o5f^1)5am&!azs7631u%z!-^0!9$@v4#e)(4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["finalyrprojectb9.herokuapp.com"]
 
 
 # Application definition
@@ -121,5 +122,6 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+django_heroku.settings(locals())
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
